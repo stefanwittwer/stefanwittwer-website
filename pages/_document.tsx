731e7b59ed60be6e44bec92ec5a5c40efc198a97
Stefan Wittwer <stefan.wittwer@nextbusiness.ch>
2022-07-29 08:01:04 +0200

    Removed duplicated viewport tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,6 @@ import React from "react";
 const Document = () => (
   <Html lang="en">
     <Head>
-      <meta
-        name="viewport"
-        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-      />
       <link rel="icon" href="/favicon.png" />
     </Head>
     <body>
